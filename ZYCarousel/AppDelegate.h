050a85a76109a07f27a785ae8097b94fd107c119
LeMo-test <lemo-test@lemo-testdeimac.local>
2016-06-12 08:32:30 +0800

    Initial Commit

---
+++ ZYCarousel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZYCarousel
+//
+//  Created by LeMo-test on 16/6/12.
+//  Copyright © 2016年 LeMo-test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ZYCarousel/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ZYCarousel
+//
+//  Created by LeMo-test on 16/6/12.
+//  Copyright © 2016年 LeMo-test. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
